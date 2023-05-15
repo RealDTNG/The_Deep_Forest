a01c7307e3abc566fb6c0309fa8e4409c5b8e39a
RealDTNG <ddwh2005@gmail.com>
2023-05-15 12:12:00 -0400

    to do's

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Dawson Hoyle + Dylan Baker
+2D Platformer, Name:TBD
+Start: 5/16/2023     End: N/A
+
+Dawson To Do List;
+    - Start/Menu
+    - Database
+    - Animations
+    - Ability Unlock
+    - Level Design
+    - Game Map
+    - Game Ending
+    - Bug Fixing/ Testing
+    
+Dylan To Do List;
+    - Menu Settings
+    - Resolution
+    - Sprites
+    - Camera
+    - Sound
+    - Test Map
+    - Game Map
+    - Game Ending
+    - Bug Fixing/ Testing
+
+
+"""
