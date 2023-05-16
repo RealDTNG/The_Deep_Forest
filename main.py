fff2641cecd0f785c782d75311e02d3583ee857f
RealDTNG <ddwh2005@gmail.com>
2023-05-16 11:10:01 -0400

    imgs

--- main.py
+++ main.py
@@ -27,9 +27,34 @@ Dylan To Do List;
 
 """
 
-import pygame as pg
+import pygame as pg, sys,img
+from button_class import Button
 
 
 pg.init()
 game_state = '__menu__'
 FPS = 60
+fpsClock = pg.time.Clock()
+WINDOW_WIDTH = 1440
+WINDOW_HEIGHT = 900
+WINDOW = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pg.HWSURFACE)
+pg.display.set_caption("Game")
+
+btn_start = Button(30, 300, 300, 100, 'Start')
+
+def display():
+    WINDOW.fill((255,255,255)) #White background
+
+    pg.Surface.blit(WINDOW,img.menu_backdrop,(0,0))
+   
+
+while True:
+    display()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+  
+
+    pg.display.update() #update the display
+    fpsClock.tick(FPS) #speed of redraw
