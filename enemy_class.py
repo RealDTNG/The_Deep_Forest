30adf3a4cf9e9d3334eb8c694f871251c3aa11f3
Aradoxis <99507343+Aradoxis@users.noreply.github.com>
2023-05-16 13:09:46 -0400

    classes

--- enemy_class.py
+++ enemy_class.py
@@ -1,47 +1,37 @@
 import pygame
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, startX,startY,width,height,image_load,img_dmg,health,double_jump_unlock):
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, startX,startY,width,height,image_load,img_dmg,health,agro_range):
         super().__init__()
         self.image = pygame.transform.scale(image_load, (width, height)).convert_alpha()
         self.mask  = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect(topleft=(startX,startY))
+        self.agro_range = agro_range
         self.w = width
         self.h = height
         self.imgld = image_load
         self.imgdmg = img_dmg
         self.hp = health
         self.movey = 0
-        if double_jump_unlock:
-            self.jump = 2
-            self.jumpcount = 2
-        else:
-            self.jump = 1
-            self.jumpcount = 1
         
-    def move(self,barriers):
+    def move(self,barriers,player):
         self.image = pygame.transform.scale(self.imgld, (self.w, self.h)).convert_alpha()
-        keyvalu = {True : 1, False: 0}
-        key_input = pygame.key.get_pressed()
         
-        self.movex = int((-0.5*(keyvalu[key_input[pygame.K_LEFT]]+keyvalu[key_input[pygame.K_a]])**2)+(1.5*(keyvalu[key_input[pygame.K_LEFT]]+keyvalu[key_input[pygame.K_a]])))*-2 + ((-0.5*(keyvalu[key_input[pygame.K_RIGHT]]+keyvalu[key_input[pygame.K_d]])**2)+(1.5*(keyvalu[key_input[pygame.K_RIGHT]]+keyvalu[key_input[pygame.K_d]])))*2
+        if ((player.rect.x + player.rect.width/2 - (self.rect.x + self.w))**2 + (player.rect.y + player.rect.height/2 - (self.rect.y + self.h))**2)**(1/2) <= self.agro_range:
+            self.movex = 2(player.rect.x-self.rect.x)/abs(player.rect.x-self.rect.x)
+        
         self.rect.x += self.movex
-        
+
         if self.rect.colliderect(b.rect for b in barriers):
             self.rect.x -= self.movex
 
         self.movey+=1
-
-        if key_input[pygame.K_SPACE] and self.jump >0:
-            self.movey -= 10
-            self.jump -= 1
         
         self.rect.y += self.movey
         
         for b in barriers:
             if self.rect.colliderect(b.rect):
                 self.y = b.rect.y + self.h
-                self.jump+=self.jumpcount
 
     def hit(self,dmg):
         self.hp -= dmg
--- sword_class(WIP).py
+++ sword_class(WIP).py
@@ -1,8 +1,11 @@
 import pygame
 
-class Barrier(pygame.sprite.Sprite):
+class Sword(pygame.sprite.Sprite):
     def __init__(self, startX,startY,width,height,image_load):
         super().__init__()
         self.image = pygame.transform.scale(image_load, (width, height)).convert_alpha()
         self.mask  = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect(topleft=(startX,startY))
+
+    def process(self,angle):
+        self.rect = pygame.transform.rotate(self.rect, angle)
