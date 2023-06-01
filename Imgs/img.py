e0b3d077471d7388c7efdba113ab97f999b35788
RealDTNG <ddwh2005@gmail.com>
2023-06-01 11:04:23 -0400

    imgs

--- Imgs/img.py
+++ Imgs/img.py
@@ -2,7 +2,12 @@
 File TO Store Imgs Neatly
 """
 import pygame as pg
+from spritesheet import spritesheet
 
-
-menu_backdrop = pg.transform.scale(pg.image.load('Imgs/menu_backdrop.png'),(1440,900))
-grass = pg.transform.scale(pg.image.load('Imgs/grass.jfif'),(1440,200))
+def imgs():
+    global tree_sheet, menu_backdrop, grass, logg, tree1
+    tree_sheet = spritesheet('Imgs\Tree_SpriteSheet_Outlined.png')
+    menu_backdrop = pg.transform.scale(pg.image.load('Imgs/menu_backdrop.png'),(1440,900))
+    grass = pg.transform.scale(pg.image.load('Imgs/grass.jfif'),(1440,200))
+    logg = tree_sheet.image_at((314,97,12,31))
+    tree1 = tree_sheet.image_at((0, 41, 71, 86))
