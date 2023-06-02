792e7c745bdfc6e7a928d76b74ae28a5e9292841
Dawson Hoyle <ddwh2005@gmail.com>
2023-06-02 10:31:41 -0400

    imgs

--- Imgs/img.py
+++ Imgs/img.py
@@ -5,9 +5,12 @@ import pygame as pg
 from spritesheet import spritesheet
 
 def imgs():
-    global tree_sheet, menu_backdrop, grass, log, tree1
-    tree_sheet = spritesheet('Imgs\Tree_SpriteSheet_Outlined.png')
+    global tree_sheet, menu_backdrop, grass, log, tree1, big_rock, rock, thin_grass
+    tree_sheet = spritesheet('Imgs\Tree_SpriteSheet.png')
     menu_backdrop = pg.transform.scale(pg.image.load('Imgs/menu_backdrop.png'),(1440,900))
-    grass = pg.transform.scale(pg.image.load('Imgs/grass.jfif'),(1440,200))
-    logg = tree_sheet.image_at((834,88,21,33))
+    grass = tree_sheet.image_at((671,136,97,30))
+    log = tree_sheet.image_at((834,88,21,33))
+    big_rock = tree_sheet.image_at((616,200,44,34))
+    rock = tree_sheet.image_at((569,216,30,16))
+    thin_grass = tree_sheet.image_at((569,144,96,17))
     tree1 = tree_sheet.image_at((0, 41, 71, 86))
